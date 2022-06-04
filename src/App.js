2be014b2b15c2972a1f148a9ae79cccb6110ae1f
Phu Quang <86101765+phuquang147@users.noreply.github.com>
2022-06-04 23:26:16 +0700

    Setup routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { DefaultLayout } from '~/components/Layout';
+import { publicRoutes } from '~/routes';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">ád</header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            const Page = route.component;
+            let Layout = DefaultLayout;
+
+            if (route.layout) Layout = route.layout;
+            else if (route.layout === null) Layout = Fragment;
+
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
